c5bb241351cce8c18816225d0e733c9e242c95cc
desynabilasari <desynabilasari98@gmail.com>
2025-09-11 16:41:01 +0700

    DataDiri

---
+++ src/modul_1/tipedata/DataDiri.java
@@ -0,0 +1,24 @@
+package modul_1.tipedata;
+
+public class DataDiri {
+    public static void main(String[] args) {
+        String nama, alamat;
+        int usia;
+        double tinggibadan;
+        float beratbadan;
+        char jk;
+
+        nama = "Desi Nabila Sari";
+        alamat = "Lhokseumawe";
+        usia = 50;
+        tinggibadan = 15.3;
+        beratbadan = 50;
+        jk = "p";
+
+        System.out.println("nama:" + nama);
+        System.out.println("alamat:" + alamat);
+        System.out.println("tinggibadan:" + tinggibadan);
+        System.out.println("beratbadan:" + beratbadan);
+        System.out.println("jk:" + jk);
+    }
+}
